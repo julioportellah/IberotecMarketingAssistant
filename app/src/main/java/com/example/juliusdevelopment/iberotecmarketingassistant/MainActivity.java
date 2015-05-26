740ffab071aa75918f716a8aca84e05db3c665dd
julioportellah <julioportellah@gmail.com>
2015-05-25 19:11:15 -0500

    Adding a registration activity

--- app/src/main/java/com/example/juliusdevelopment/iberotecmarketingassistant/MainActivity.java
+++ app/src/main/java/com/example/juliusdevelopment/iberotecmarketingassistant/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.MediaController;
 public class MainActivity extends ActionBarActivity {
 
 
-    Button videoButton1, videoButton2, videoButton3;
+    Button videoButton1, videoButton2, videoButton3,videoButton4,videoButton5,videoButton6;
     Button registrationButton;
     //Button registerButton;
     MediaController mdc1,mdc2;
@@ -34,23 +34,29 @@ public class MainActivity extends ActionBarActivity {
         videoButton1=(Button)findViewById(R.id.video_btn_1);
         videoButton2=(Button)findViewById(R.id.video_btn_2);
         videoButton3=(Button)findViewById(R.id.video_btn_3);
+        videoButton4=(Button)findViewById(R.id.video_btn_4);
+        videoButton5=(Button)findViewById(R.id.video_btn_5);
+        videoButton6=(Button)findViewById(R.id.video_btn_6);
         registrationButton=(Button)findViewById(R.id.registro_btn);
 
         videoButton1.setOnClickListener(centralHandler);
         videoButton2.setOnClickListener(centralHandler);
         videoButton3.setOnClickListener(centralHandler);
+        videoButton4.setOnClickListener(centralHandler);
+        videoButton5.setOnClickListener(centralHandler);
+        videoButton6.setOnClickListener(centralHandler);
         registrationButton.setOnClickListener(centralHandler);
         //Fin de la definicion de botones
 
         videoDeMuestra=(VideoView) findViewById(R.id.videoView);
-        videoDeMuestra2=(VideoView) findViewById(R.id.videoView);
-        videoDeMuestra3=(VideoView) findViewById(R.id.videoView);
+        //videoDeMuestra2=(VideoView) findViewById(R.id.videoView);
+        //videoDeMuestra3=(VideoView) findViewById(R.id.videoView);
 
 
         String path1="android.resource://"+getPackageName()+"/"+R.raw.video_frecuencia_latina;
         //String path2="android.resource://"+getPackageName()+"/"+R.raw.video_globo;
         mdc1=new MediaController(this);
-        mdc2=new MediaController(this);
+        //mdc2=new MediaController(this);
         videoDeMuestra.setVideoURI(Uri.parse(path1));
         //videoDeMuestra.setVideoPath(path1);
         mdc1.setAnchorView(videoDeMuestra);
@@ -69,6 +75,16 @@ public class MainActivity extends ActionBarActivity {
             case 3:
                 finalString = "android.resource://" + getPackageName() + "/" + R.raw.drone_construido;
                 break;
+            case 4:
+                finalString = "android.resource://" + getPackageName() + "/" + R.raw.video_superpoderes;
+                break;
+            case 5:
+                finalString = "android.resource://" + getPackageName() + "/" + R.raw.video_telecomunicaciones;
+                break;
+            case 6:
+                finalString = "android.resource://" + getPackageName() + "/" + R.raw.video_teleamor;
+                break;
+
         }
         return finalString;
     }
@@ -103,6 +119,21 @@ public class MainActivity extends ActionBarActivity {
                 //playVideoTest(2);
                 case R.id.video_btn_3:
                     videoString=getFileName(3);
+                    playVideo(videoString);
+                    break;
+                //playVideoTest(3);
+                case R.id.video_btn_4:
+                    videoString=getFileName(4);
+                    playVideo(videoString);
+                    break;
+                //playVideoTest(3);
+                case R.id.video_btn_5:
+                    videoString=getFileName(5);
+                    playVideo(videoString);
+                    break;
+                //playVideoTest(3);
+                case R.id.video_btn_6:
+                    videoString=getFileName(6);
                     playVideo(videoString);
                     break;
                 //playVideoTest(3);
